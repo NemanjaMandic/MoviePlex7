6a17a00d8bcf316a7a2eef9216e4e856aed82644
NemanjaMandic <nemanjans84@gmail.com>
2017-02-01 01:13:27 +0100

    ClientBackingBean

--- target/movieplex7-1.0-SNAPSHOT/chat/websocket.js
+++ target/movieplex7-1.0-SNAPSHOT/chat/websocket.js
@@ -1,55 +1,65 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-var wsUri = 'ws://' + document.location.host
-               + document.location.pathname.substr(0,
-                 document.location.pathname.indexOf("/faces"))
-               + '/websocket';
-       console.log(wsUri);
-       
-var websocket = new WebSocket(wsUri);
+var websocketUri = 'ws://' + document.location.host
+        + document.location.pathname.substr(0,
+                document.location.pathname.indexOf("/faces")) + '/websocket';
+console.log(websocketUri);
+var websocket = new WebSocket(websocketUri);
 
 var textField = document.getElementById("textField");
 var users = document.getElementById("users");
 var chatlog = document.getElementById("chatlog");
 
 var username;
-websocket.onopen = function(evt) { onOpen(evt); };
-websocket.onmessage = function(evt) { onMessage(evt); };
-websocket.onerror = function(evt) { onError(evt); };
-websocket.onclose = function(evt) { onClose(evt); };
+
+websocket.onopen = function (event) {
+    onOpen(event);
+};
+websocket.onmessage = function (event) {
+    onMessage(event);
+};
+websocket.onerror = function (event) {
+    onError(event);
+};
+websocket.onclose = function (event) {
+    onClose(event);
+};
+
 var output = document.getElementById("output");
 
 function join() {
     username = textField.value;
     websocket.send(username + " joined");
 }
+
 function send_message() {
     websocket.send(username + ": " + textField.value);
 }
+
 function onOpen() {
     writeToScreen("CONNECTED");
 }
+
 function onClose() {
     writeToScreen("DISCONNECTED");
 }
- function onMessage(evt) {
-    writeToScreen("RECEIVED: " + evt.data);
-    if (evt.data.indexOf("joined") !== ?1) {
-      users.innerHTML += evt.data.substring(0, evt.data.indexOf(" joined")) + "\n";
+
+function onMessage(event) {
+    writeToScreen("RECEIVED: " + event.data);
+
+    if (event.data.indexOf("joined") !== -1) {
+        users.innerHTML += event.data.substring(0,
+                event.data.indexOf("joined")) + "\n";
     } else {
-      chatlog.innerHTML += evt.data + "\n";
-   }
+        chatlog.innerHTML += event.data + "\n";
+    }
 }
-function onError(evt) {
-  writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+
+function onError(event) {
+    writeToScreen('<span style="color: red;">ERROR:</span> '
+            + event.data);
 }
+
 function disconnect() {
-   websocket.close();
+    websocket.close();
 }
 
 function writeToScreen(message) {
